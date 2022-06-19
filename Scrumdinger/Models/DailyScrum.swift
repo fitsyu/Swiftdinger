83680ad7af599ea0165e9952213e695a3442b7f0
Fitsyu Two <fitsyu2@gmail.com>
2022-06-19 13:20:28 +0700

    Creating a Card View

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,21 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Fitsyu  on 19/06/22.
+//
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] = [
+        DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, theme: .yellow),
+        DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .orange),
+        DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy)
+    ]
+}
